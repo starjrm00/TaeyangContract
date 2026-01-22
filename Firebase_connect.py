ef828d977083216603cd65bdb6088a44320940ec
unknown <starjrm00@gmail.com>
2026-01-22 15:51:41 +0900

    split json parsing logic local/streamlit_releasee

--- Firebase_connect.py
+++ Firebase_connect.py
@@ -1,16 +1,16 @@
 import firebase_admin
+import streamlit as st
 import json
 from firebase_admin import credentials, firestore
 import os
 
-#for local
-cred = credentials.Certificate("firebase_service_key.json")
-
-#for distribution
-#cred = json.loads(st.secrets["firebase_service_key"])
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+if os.path.exists("firebase_service_key.json"):
+    #for local
+    cred = credentials.Certificate("firebase_service_key.json")
+else:
+    #for release
+    cred_dict = json.loads(st.secrets["firebase_service_key"])
+    cred = credentials.Certificate(cred_dict)
 
 db = firestore.client(database_id = "(default)")
 
