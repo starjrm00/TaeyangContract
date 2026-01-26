830ca322dcb2b1eb24dd303be1c13790c8de94df
unknown <starjrm00@gmail.com>
2026-01-27 07:55:01 +0900

    Set notification from 9AM to 10AM

--- Notification/main.py
+++ Notification/main.py
@@ -16,7 +16,7 @@ def notify_contracts(request: Request):
 
     calendar = build("calendar", "v3", credentials=credentials)
 
-    alert_settings = [30, 7, 1]
+    alert_settings = [28, 7, 1]
     trade_query = db.collection("Trade").where("calendar_notify", "==", False)
     trades = trade_query.stream()
 
@@ -26,15 +26,24 @@ def notify_contracts(request: Request):
         trade_company = data["trade_company"]
         trade_item = data["trade_item"]
         end_date = data["trade_end_date"].date()
+        start_dt = datetime.datetime.combine(end_date, datetime.time(9, 0))
+        end_dt = datetime.datetime.combine(end_date, datetime.time(10, 0))
         reminders = [{"method": "popup", "minutes": 60*24*days} for days in alert_settings]
+        print(reminders)
 
         print(f"{trade_company}와 거래하는 {trade_item}의 종료일은 {end_date}입니다")
 
         event = {
             "summary": f"계약 종료 예정: {trade_company}, {trade_item}",
             "description": "계약 종료 전 팝업알림, 계약이 변동되었다면 삭제해주세요",
-            "start": {"date": end_date.isoformat()},
-            "end": {"date": end_date.isoformat()},
+            "start": {
+                "dateTime": start_dt.isoformat(),
+                "timeZone": "Asia/Seoul"
+            },
+            "end": {
+                "dateTime": end_dt.isoformat(),
+                "timeZone": "Asia/Seoul"
+            },
             "reminders": {
                 "useDefault": False,
                 "overrides": reminders
